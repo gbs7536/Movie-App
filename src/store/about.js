bd5a4a27881465327de9078267fc4337941decd1
gbs7536 <gbs7536@naver.com>
2025-04-26 18:26:12 +0900

    Create project

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+import { Store } from '../core/reina'
+
+export default new Store({
+  photo: 'https://avatars.githubusercontent.com/u/138460840?v=4',
+  name: 'Reina / gbs7536',
+  email: 'test@gmail.com',
+  blog: 'https://gbs7536.tistory.com/',
+  github: 'https://github.com/gbs7536',
+  repository: 'https://github.com/gbs7536/Movie-App'
+})
