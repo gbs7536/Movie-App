6d33d3988fb5bbd9dfbd5e9b75998066287c0265
gbs7536 <gbs7536@naver.com>
2025-04-29 21:39:18 +0900

    Compiled Typescript

--- src/store/about.ts
+++ src/store/about.ts
@@ -1,10 +1,19 @@
 import { Store } from '../core/reina'
 
-export default new Store({
+interface State {
+  photo: string
+  name: string
+  email: string
+  blog: string
+  github: string
+  repository: string
+}
+
+export default new Store<State>({
   photo: 'https://avatars.githubusercontent.com/u/138460840?v=4',
   name: 'Reina / gbs7536',
   email: 'test@gmail.com',
   blog: 'https://gbs7536.tistory.com/',
   github: 'https://github.com/gbs7536',
-  repository: 'https://github.com/gbs7536/Movie-App'
+  repository: 'https://github.com/gbs7536/Movie-App',
 })
